598a774b3ac3071a39a3c2b482a416b47ccfc0d4
LDarioPL <ldariopadilla@gmail.com>
2025-11-13 09:27:59 -0600

    First commit!

---
+++ src/controlmaritimo/Barcos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlmaritimo;
+
+/**
+ *
+ * @author Dario Padilla
+ */
+public class Barcos {
+    System.out.print("Hello poopoo");
+}
